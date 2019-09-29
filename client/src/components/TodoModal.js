9fc5ae37554328fda45ec9fa2ad08a4c0ef8a4ad
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2019-09-29 19:34:58 +0530

    changes

--- client/src/components/TodoModal.js
+++ client/src/components/TodoModal.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react'
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import PropTypes from 'prop-types';
 
 
 export class TodoModal extends Component {
+  
+  
+  propTypes = {
+    toggle: PropTypes.func.isRequired,
+    modal: PropTypes.bool,
+  };
+
   render() {
     return (
       <div>
-        
+        <Modal isOpen={this.props.modal} toggle={this.props.toggle} >
+          <ModalHeader toggle={this.props.toggle}>{this.props.todo.name}</ModalHeader>
+          <ModalBody>
+          {this.props.todo.description}
+          {this.props.todo.status}
+          </ModalBody>
+          <ModalFooter>
+            <Button color="secondary" onClick={this.props.toggle}>Close</Button>
+          </ModalFooter>
+        </Modal>
       </div>
     )
   }
