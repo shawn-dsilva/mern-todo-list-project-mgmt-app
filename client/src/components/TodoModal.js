dbd8931d66994b666245ab7d2f208eb61b54866a
shawn-dsilva <shawn.dsilva_97@protonmail.com>
2019-09-29 19:07:06 +0530

    todo modal start

---
+++ client/src/components/TodoModal.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+
+export class TodoModal extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
+
+export default TodoModal
